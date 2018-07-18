29118b621e46dd9f8b6154987ccd9702a90f1e0d
katongole <katsisaac50@gmail.com>
2018-07-18 12:38:39 +0200

    Header file created

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+import './index.css'
+
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+     <div className="row banner">
+         <div className="banner-text">
+            <h1 >The African Music Archive</h1>
+            <h3>One point destination for all your favorite</h3>
+            <hr />
+            
+         </div>
+      </div>
+   </header>
+    );
+  }
+}
+
+export default Header;
