3266077953e4e0162d4b2de41fd42976031b5033
katongole <katsisaac50@gmail.com>
2018-08-28 11:02:16 +0200

    Signup removed

--- client/src/Container/Nav.js
+++ client/src/Container/Nav.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import {  CategorySearch,SingleDropdownList } from '@appbaseio/reactivesearch';
 import 'semantic-ui-css/semantic.min.css';
 import '../index.css'
-import Signup from './Signup';
 
 
 
@@ -13,15 +11,12 @@ class Nav extends Component {
                 
             <div class='topnav'>
             <a class='active' href='/'>Home</a>
-            <a href='#about'>About</a>
-            <a href='#contact'>Contact</a>
+            <a href='/About'>About</a>
+            <a href='/contact'>Contact</a>
             {/* <input type="text" placeholder="Search.."/> */}
             <div style={{ display: "flex", flexDirection: "row-reverse"}}>
               </div>
-              <div class='signup'>
-                <a class='cbtm'>Sign&nbsp;in</a>
-                <a class='cbtn' href="Signup">Sign&nbsp;up</a>
-              </div>
+              
           </div>
         );
     }
