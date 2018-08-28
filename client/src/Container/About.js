38406991e28ead9e915f7541efabbb1a39467568
katongole <katsisaac50@gmail.com>
2018-08-28 11:01:08 +0200

    About page made

---
+++ client/src/Container/About.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+
+class About extends Component {
+  
+  render () {
+    
+    return (
+
+        <div>
+<h1>This is the about page</h1>
+
+        </div>
+    )
+    
+  }
+}
+
+export class Contact extends Component {
+  
+  render () {
+    
+    return (
+
+        <div>
+<h1>This is the contact page</h1>
+
+        </div>
+    )
+    
+  }
+}
+
+export default About
