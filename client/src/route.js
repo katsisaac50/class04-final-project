f2802c6f36fff60c665ac5de251efa07ce077dd6
katongole <katsisaac50@gmail.com>
2018-08-28 11:00:04 +0200

    route for about and contact added!

--- client/src/route.js
+++ client/src/route.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import About, {Contact}from './Container/About'
 import AlbumPreview from './Container/AlbumPreview';
 import { Route } from "react-router-dom";
 import 'semantic-ui-css/semantic.min.css';
-import Signup from './Container/Signup';
 
 class Route1 extends Component {
   render() {
@@ -11,10 +11,12 @@ class Route1 extends Component {
       
       <div>
         <Route exact path = "/" component ={Home}/>
+        <Route exact path = "/About" component ={About}/>
+        <Route exact path = "/Contact" component ={Contact}/>
         {/* <Route  exact path="/AlbumList" component = {AlbumPreview} /> */}
         <Route   path="/AlbumPreview/:id" component = {AlbumPreview} />
-        <Route   path="/Signup" component = {Signup} />
-
+{/*         <Route   path="/Signup" component = {Signup} />
+ */}
       </div>
     );
   }
