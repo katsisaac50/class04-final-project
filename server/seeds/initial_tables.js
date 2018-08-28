9dedce4ce9d813b7e8f001a7cc2a666b0d977e06
katongole <katsisaac50@gmail.com>
2018-08-28 11:08:19 +0200

    creating foreign key coloumn

--- server/seeds/initial_tables.js
+++ server/seeds/initial_tables.js
@@ -1,43 +1,55 @@
-const inputData = require(__dirname + '/../data/output.json');
+const inputData = require(__dirname + '/../data/output.json')
 
 // const recordNumber = 50
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('Record').del()
-    .then(function () {
 
-      let records = [];
-      let id = 1;
+    .then(function () {
+      let records = []
+      let id = 1
 
       // Inserts seed entries
       for (let key in inputData) {
-        records.push({id_record: id++, title: inputData[key].title});//ids are set manually here so we know where there.
+        records.push({id_record: id++, title: inputData[key].title}); // ids are set manually here so we know where there.
       }
       console.log(records)
-      return   knex('Record')
-              .insert(records)
-              .then(function (row) {        
+      return knex('Record')
+        .insert(records)
 
-            let songsList = [];
-            let id = 1; 
-            for (let key in inputData) {
-  
-              console.log(key);
-  
-              for (track in inputData[key].tracks.all) {
-                songsList.push( {
-                   title: inputData[key].tracks.all[track],
-                   record_idrecord: id
-                 });           
-               }
-               id=id+1;
+        .then(function (row) {
+          let songsList = []
+          let id = 1
+          for (let key in inputData) {
+            console.log(key)
+
+            for (track in inputData[key].tracks.all) {
+              songsList.push({
+                title: inputData[key].tracks.all[track],
+                record_idrecord: id
+              })
             }
+            id = id + 1
+          }
 
-              return   knex('Song')
-              .insert(songsList);
- 
+          return knex('Song')
+            .insert(songsList)
+            
+        })
+        
+        // .then(function () {
 
-            }).catch(err => console.error(err));
-
-    });
-};
+        //   let likes = [];
+        //   let id = 1;
+    
+        //   // Inserts seed entries
+        //   for (let key in inputData) {
+        //     records.push({id_record: id++, title: inputData[key].title});//ids are set manually here so we know where there.
+        //   }
+        //   console.log(records)
+        //   return   knex('Record')
+        //           .insert(records)
+        //         })
+        // .catch(err => console.error(err))
+    })
+}
