4fe1d591a7a9c1106a419de6a9863e612167acd0
katongole <katsisaac50@gmail.com>
2018-09-02 15:29:03 +0200

    fixed bugs

--- server/routes/song.js
+++ server/routes/song.js
@@ -4,10 +4,10 @@ var router = express.Router();
 var knex  = require('../helpers/knex');
 
 /* GET category listing. */ 
-router.get("/:songid", function(req, res, next) {
+router.get("/:songtitle", function(req, res, next) {
   console.log(req.params.songid) 
 
-  knex('Song').select().where('title', req.params.songid).then( function(data) {
+  knex('Song').select().where('title', req.params.songtitle).then( function(data) {
     res.send(data);
   });
 });
@@ -15,15 +15,15 @@ router.get("/:songid", function(req, res, next) {
 
 
 //likes +1  route
-router.put("/:songid", function (request, res) {
+router.put("/:songtitle", function (request, res) {
 
 console.log(request)
 
   knex('Song')
-  .where('id_song', request.params.songid)
+  .where('title', request.params.songtitle)
   .increment('likes', 1).then(function(data) {
     knex('Song')
-    .where('id_song', request.params.songid)
+    .where('title', request.params.songtitle)
     .then( function(data2) {
       res.send(data2.likes);
     });
@@ -33,12 +33,12 @@ console.log(request)
 
 
 //likes -1  route
-router.delete("/:songid", function (request, res) {
+router.delete("/:songtitle", function (request, res) {
   knex('Song')
-  .where('id_song', request.params.songid)
+  .where('title', request.params.songtitle)
   .decrement('likes', 1).then( function(data) {
     knex('Song')
-    .where('id_song', request.params.songid)
+    .where('title', request.params.songtitle)
     .then( function(data2) {
       res.send(data2.likes);
     });
